950e279b6eba1d0815d5ad0dba48ef6fe40a0335
dapsman <dapo.ogunlana@kbcoy.com.ng>
2018-08-31 14:58:08 +0100

    3pm friday commit

--- resources/views/customer/request.blade.php
+++ resources/views/customer/request.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.backend')
 
 @section('content')
-<div class="col-sm-12 row bgground">
+<div class="col-sm-12 row bgground reqimg">
+    <div class="col-sm-12 spr"></div>
     <div class="col-sm-4 "></div>
-    <div class="col-sm-4">
+    <div class="col-sm-4 reqinn">
         <div class="container" style="padding-top: 50px;">
             <h5 style="text-align: center">Type of Trip</h5>
             <div class="row">
