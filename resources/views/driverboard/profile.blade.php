f4332a195ab12dbc4071f9a7eb10164ec2acc5fd
dapsman <dapo.ogunlana@kbcoy.com.ng>
2018-09-04 17:47:49 +0100

    merging master complete

--- resources/views/driverboard/profile.blade.php
+++ resources/views/driverboard/profile.blade.php
@@ -1,24 +1,15 @@
 @extends('layouts.driverboard')
 
 @section('content')
-<<<<<<< HEAD
 <div class="container">
   <p>YOUR PROFILE</p>
 	<div class="row filler">
    <div class="col-md-6">
-=======
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<div class="container w3-light-grey w3-padding-16">
-  <p><h5>YOUR PROFILE</h5></p>
-  <div class="row">
-   <div class="col-sm-6">
->>>>>>> 4044ba64b90abd5439e9645e3c106c01e0124a3f
     @if(session()->has('update'))
     <div class="alert alert-success">
       {{ session()->get('update') }}
     </div>
     @endif
-<<<<<<< HEAD
       <div class="prof-div">
         <img src="{{$user->picture}}"  style="width:100%; border-radius:50%; border:2px solid #666;"/>
       </div><br/><br/>
@@ -33,20 +24,6 @@
     <p><b>State : {{$user->state}} </b></p>
     <p><b>D.O.B : {{$user->d_o_b}} </b></p>
     <p><b>Nationality : {{$user->nationality}} </b></p>
-=======
-    <img src="{{$user->picture}}" width="500">
-  </div>
-  <div class="col-sm-6">
-    <p><b>First Name: <i>{{$user->f_name}} </i></b></p>
-    <p><b>Last Name : <i>{{$user->l_name}} </i></b></p>
-    <p><b>Phone Number: <i>{{$user->phone_no}}</i> </b></p>
-    <p><b>Email : <i>{{$user->email}} </i></b></p>
-    <p><b>Bank Name : <i>{{$user->bank_name}} </i></b></p>
-    <p><b>Bank Account : <i>{{$user->bank_account}} </i></b></p>
-    <p><b>State : <i>{{$user->state}} </i></b></p>
-    <p><b>D.O.B : <i>{{$user->d_o_b}} </i></b></p>
-    <p><b>Nationality : <i>{{$user->nationality}} </i></b></p>
->>>>>>> 4044ba64b90abd5439e9645e3c106c01e0124a3f
     @if($user->status == 0)
     <p><b>Status : </b><b style="color:blue">Available for Service</b> </p>
     @else
